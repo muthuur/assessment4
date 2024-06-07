2418d6dd9eda51719c7706615043d290ae18cd8b
muthuur <sammymuthuurikinyua>
2024-06-07 19:51:11 +0300

    update routes

--- src/interfaces/NotesService.ts
+++ src/interfaces/NotesService.ts
@@ -1,10 +1,9 @@
 import { Note } from "./Note";
-import { Res } from "./Res";
 
 export interface NotesService {
-  getNotes(): Promise<Res>;
-  getNote(id: string): Promise<Res>;
-  createNote(note: Note): Promise<Res>;
-  updateNote(id: string, note: Note): Promise<Res>;
-  deleteNote(id: string): Promise<Res>;
+  getNotes(): Promise<{}>;
+  getNote(id: string): Promise<{}>;
+  createNote(note: Note): Promise<{}>;
+  updateNote(id: string, note: Note): Promise<{}>;
+  deleteNote(id: string): Promise<{}>;
 }
